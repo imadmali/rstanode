5eca540e3e655fd295ce0261eece7224f4d3680d
Imad Ali <imadmali@gmail.com>
2017-07-15 21:27:38 -0400

    reintroduce tests; adding example to README.md

--- README.md
+++ README.md
@@ -3,12 +3,75 @@
 [![Build Status](https://travis-ci.org/imadmali/rstanode.svg?branch=master)](https://travis-ci.org/imadmali/rstanode)
 [![codecov](https://codecov.io/gh/imadmali/rstanode/branch/master/graph/badge.svg)](https://codecov.io/gh/imadmali/rstanode)
 
-An ODE wrapper for [RStan](https://github.com/stan-dev/rstan) which takes a R function representation of an ODE system of equations and generates simulates via RStan.
+An ODE wrapper for [RStan](https://github.com/stan-dev/rstan) which takes a R function representation of an ODE system of equations and generates simulations via RStan. ~~There is also an option to fit data to the ODE system and estimate the initial conditions and/or the parameters.~~
 
 ### Installation
 
-To clone the repository and build the package locally fun the following from R:
+To clone the repository and build the package locally run the following from R:
 
-```
+```r
 devtools::install_github("imadmali/stanode")
 ```
+
+### Example
+
+In this example we simulate an [Arenstorf Orbit](https://en.wikipedia.org/wiki/Richard_Arenstorf#The_Arenstorf_Orbit).
+
+First define the ODE system as an R function (in this example we are using syntax that corresponds to the R package [deSolve](https://cran.r-project.org/web/packages/deSolve/index.html))
+
+```r
+Arenstorf <- function(t, y, p) {
+  with(as.list(c(y,p)), {
+    D1 <- ((y[1] + mu1)^2 + y[2]^2)^(3/2)
+    D2 <- ((y[1] - mu2)^2 + y[2]^2)^(3/2)
+    dy1 <- y[3]
+    dy2 <- y[4]
+    dy3 <- y[1] + 2*y[4] - mu2*(y[1]+mu1)/D1 - mu1*(y[1]-mu2)/D2
+    dy4 <- y[2] - 2*y[3] - mu2*y[2]/D1 - mu1*y[2]/D2
+    return(list( c(dy1, dy2, dy3, dy4) ))
+  })
+}
+```
+
+Next specify the initial state values, parameter values, and time steps.
+
+```r
+mu1 <- 0.012277471
+pars <- c(mu1 = mu1, mu2 = 1 - mu1)
+yini <- c(y1 = 0.994, y2 = 0,
+          y3 = 0, y4 = -2.00158510637908252240537862224)
+time_steps <- seq(from = 0, to = 18, by = 0.01)
+```
+
+Now we can simulate state values from the model at each time step with `rstanode::stan_ode`.
+
+```r
+orbit_rstanode <- stan_ode(Arenstorf,
+                           yini,
+                           pars,
+                           time_steps,
+                           integrator = "rk45")
+sims <- orbit_rstanode$simulations
+```
+
+To check we also simulate using `deSolve::ode`.
+
+```r
+orbit_deSolve <- ode(func = Arenstorf, y = yini, parms = pars, times = time_steps)
+```
+
+Overlaying both results for state variable `y1` and `y2` we have the following plot.
+
+```r
+plot(orbit_deSolve[,2], orbit_deSolve[,3], type = "l", col = "#336688", lwd = 5,
+     main = "Arenstorf Orbit", xlab = expression(y[1]), ylab = expression(y[2]))
+lines(sims[,2], sims[,3], col = "#FF6688", lwd = 2)
+legend("bottomright", c("rstanode", "deSolve"), col = c("#FF6688","#336688"),
+       pch = c(15,15), pt.cex = 2, bty = "n")
+```
+
+<div style="text-align:center">
+<a href="https://raw.githubusercontent.com/imadmali/rstanode/master/Arenstorf.pdf">
+<img src=https://raw.githubusercontent.com/imadmali/rstanode/master/Arenstorf.pdf width=50%/>
+</a>
+</div>
--- tests/testthat/test_events.R
+++ tests/testthat/test_events.R
@@ -1,81 +1,81 @@
-# library(rstanode)
-# library(deSolve)
-# 
-# context("Test events run and accuracy (rk45 only)")
-# 
-# f1 <- function(t, y, parms) {
-#   with(as.list(c(y,parms)), {
-#     dy = -k * y
-#     return(list(c(dy = dy)))
-#   })
-# }
-# 
-# f2 <- function(t, y, parms) {
-#   with(as.list(c(y,parms)), {
-#     dy1 <- y2
-#     dy2 <- -y1 - theta1 * y2
-#     return(list(c(dy1 = dy1, dy2 = dy2)))
-#   })
-# }
-# 
-# test_that("Event 'add' works for 1 state ODE", {
-#   pars <- c("k" = 0.15)
-#   yini <- c("y" = 10)
-#   time_steps <- seq(1, 10, by = 0.001)
-#   events_table <- data.frame(var = "y",
-#                              time = seq(2,5,by=1),
-#                              value = 5,
-#                              method = "add")
-#   truth <- deSolve::ode(y = yini, times = time_steps, func = f1, parms = pars, events = list(data = events_table))
-#   events_table_stan <- data.frame("time" = events_table$time,
-#                                   "y" = 5,
-#                                   method = "add")
-#   fit <- stan_ode(f1, state = yini,
-#                   pars = pars,
-#                   times = time_steps,
-#                   events = events_table_stan,
-#                   integrator = "rk45",
-#                   sampling = FALSE)
-#   sims <- fit$simulations
-#   expect_equal(sims[,2], truth[,2], tolerance = 0.01)
-# })
-# 
-# test_that("Event 'add' works for multiple states ODE", {
-#   pars <- c("theta1" = 0.5)
-#   yini <- c("y1" = 2, "y2" = 5)
-#   time_steps <- seq(1,10,by=0.001)
-#   events_table <- data.frame(var = "y1",
-#                              time = 2:4,
-#                              value = 2,
-#                              method = "add")
-#   truth <- deSolve::ode(y = yini, times = time_steps, func = f2, parms = pars, events = list(data = events_table))
-#   events_table_stan <- data.frame(time = events_table$time, y1 = 2, y2 = 0, method = "add")
-#   fit <- stan_ode(f2, state = yini,
-#                   pars = pars,
-#                   times = time_steps,
-#                   integrator = "rk45",
-#                   sampling = FALSE, events = events_table_stan)
-#   sims <- fit$simulations
-#   expect_equal(sims[,2], truth[,2], tolerance = 0.01)
-#   expect_equal(sims[,3], truth[,3], tolerance = 0.01)
-# })
-# 
-# test_that("Event 'multiply' works for multiple states ODE", {
-#   pars <- c("theta1" = 0.5)
-#   yini <- c("y1" = 2, "y2" = 5)
-#   time_steps <- seq(1,10,by=0.001)
-#   events_table <- data.frame(var = "y1",
-#                              time = 2:4,
-#                              value = 1.5,
-#                              method = "multiply")
-#   truth <- deSolve::ode(y = yini, times = time_steps, func = f2, parms = pars, events = list(data = events_table))
-#   events_table_stan <- data.frame(time = events_table$time, y1 = 1.5, y2 = 1, method = "multiply")
-#   fit <- stan_ode(f2, state = yini,
-#                   pars = pars,
-#                   times = time_steps,
-#                   integrator = "rk45",
-#                   sampling = FALSE, events = events_table_stan)
-#   sims <- fit$simulations
-#   expect_equal(sims[,2], truth[,2], tolerance = 0.01)
-#   expect_equal(sims[,3], truth[,3], tolerance = 0.01)
-# })
+library(rstanode)
+library(deSolve)
+
+context("Test events run and accuracy (rk45 only)")
+
+f1 <- function(t, y, parms) {
+  with(as.list(c(y,parms)), {
+    dy = -k * y
+    return(list(c(dy = dy)))
+  })
+}
+
+f2 <- function(t, y, parms) {
+  with(as.list(c(y,parms)), {
+    dy1 <- y2
+    dy2 <- -y1 - theta1 * y2
+    return(list(c(dy1 = dy1, dy2 = dy2)))
+  })
+}
+
+test_that("Event 'add' works for 1 state ODE", {
+  pars <- c("k" = 0.15)
+  yini <- c("y" = 10)
+  time_steps <- seq(1, 10, by = 0.001)
+  events_table <- data.frame(var = "y",
+                             time = seq(2,5,by=1),
+                             value = 5,
+                             method = "add")
+  truth <- deSolve::ode(y = yini, times = time_steps, func = f1, parms = pars, events = list(data = events_table))
+  events_table_stan <- data.frame("time" = events_table$time,
+                                  "y" = 5,
+                                  method = "add")
+  fit <- stan_ode(f1, state = yini,
+                  pars = pars,
+                  times = time_steps,
+                  events = events_table_stan,
+                  integrator = "rk45",
+                  sampling = FALSE)
+  sims <- fit$simulations
+  expect_equal(sims[,2], truth[,2], tolerance = 0.01)
+})
+
+test_that("Event 'add' works for multiple states ODE", {
+  pars <- c("theta1" = 0.5)
+  yini <- c("y1" = 2, "y2" = 5)
+  time_steps <- seq(1,10,by=0.001)
+  events_table <- data.frame(var = "y1",
+                             time = 2:4,
+                             value = 2,
+                             method = "add")
+  truth <- deSolve::ode(y = yini, times = time_steps, func = f2, parms = pars, events = list(data = events_table))
+  events_table_stan <- data.frame(time = events_table$time, y1 = 2, y2 = 0, method = "add")
+  fit <- stan_ode(f2, state = yini,
+                  pars = pars,
+                  times = time_steps,
+                  integrator = "rk45",
+                  sampling = FALSE, events = events_table_stan)
+  sims <- fit$simulations
+  expect_equal(sims[,2], truth[,2], tolerance = 0.01)
+  expect_equal(sims[,3], truth[,3], tolerance = 0.01)
+})
+
+test_that("Event 'multiply' works for multiple states ODE", {
+  pars <- c("theta1" = 0.5)
+  yini <- c("y1" = 2, "y2" = 5)
+  time_steps <- seq(1,10,by=0.001)
+  events_table <- data.frame(var = "y1",
+                             time = 2:4,
+                             value = 1.5,
+                             method = "multiply")
+  truth <- deSolve::ode(y = yini, times = time_steps, func = f2, parms = pars, events = list(data = events_table))
+  events_table_stan <- data.frame(time = events_table$time, y1 = 1.5, y2 = 1, method = "multiply")
+  fit <- stan_ode(f2, state = yini,
+                  pars = pars,
+                  times = time_steps,
+                  integrator = "rk45",
+                  sampling = FALSE, events = events_table_stan)
+  sims <- fit$simulations
+  expect_equal(sims[,2], truth[,2], tolerance = 0.01)
+  expect_equal(sims[,3], truth[,3], tolerance = 0.01)
+})
